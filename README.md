74d144beb0dc059338d351085b040438d6d3f810
Jeongseup <seup87@gmail.com>
2023-09-03 03:24:17 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # simperby-client
+
+
+### clone repositry
+```bash
+
+clone_dir="DIRECTORY_WHICH_YOU_WANT_TO_DOWNLOAD"
+simperby-cli $clone_dir clone https://github.com/Simperby-Ethcon/dev-chain.git
+```
--- src/main.rs
+++ src/main.rs
@@ -21,25 +21,53 @@ async fn main() {
     println!("Hello, world!");
 
     let client = Client::open(path, Config {}, auth.clone()).await.unwrap();
-    // let test = client
+    let height = client
+        .repository()
+        .read_last_finalization_info()
+        .await
+        .unwrap()
+        .header
+        .height;
+    println!("last block height: {}", height);
+
+    // let test = client.repository().read_finalization_info(1).await.unwrap();
+
+    // let test2 = client
     //     .repository()
-    //     .read_last_finalization_info()
+    //     .read_governance_approved_agendas()
     //     .await
-    //     .unwrap()
-    //     .header;
+    //     .unwrap();
 
-    let test = client.repository().read_agendas().await.unwrap();
+    let commit_1 = "7e2c04181d63b75c1060a016c16d53fea7eb3301".to_string();
+    // let commit_2 = "7e2c04181d63b75c1060a016c16d53fea7eb3301".to_string();
 
-    match client.repository().read_blocks().await {
-        Ok(blocks) => {
-            for (commit_hash, block_hash) in blocks {
-                println!("Commit Hash: {:?}", commit_hash);
-                println!("Block Hash: {:?}", block_hash);
-            }
+    let hash = hex::decode(&commit_1).unwrap().try_into().unwrap();
+    let data = client
+        .repository()
+        .read_commit(CommitHash { hash })
+        .await
+        .unwrap();
+
+    match data {
+        Commit::Transaction(data) => {
+            let body = data.body;
+            dbg!(body);
         }
-        Err(err) => {
-            eprintln!("Error reading blocks: {:?}", err);
+        _ => {
+            println!("this is not transaction type");
         }
     }
-    // dbg!(blocks);
+
+    // dbg!("first commit", data);
+    // data.clone().
+    // let test = data.into();
+
+    let hash = hex::decode(&commit_1).unwrap().try_into().unwrap();
+    let data = client
+        .repository()
+        .read_commit(CommitHash { hash })
+        .await
+        .unwrap();
+
+    dbg!("second commit", data);
 }
